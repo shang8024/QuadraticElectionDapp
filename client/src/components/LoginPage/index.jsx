a87b798702764037d024a76e12ed483fbc3ea9de
Jiale Shang <shang8024@gmail.com>
2024-03-16 12:38:33 -0400

    import Mui for frontend LoginPage

--- client/src/components/LoginPage/index.jsx
+++ client/src/components/LoginPage/index.jsx
@@ -1,4 +1,15 @@
 import React, { useState } from "react";
+import {
+  Container
+  , Box
+  , Typography
+  , TextField
+  , FormControlLabel
+  , Checkbox
+  , Button
+  , Grid
+  , Link
+} from "@mui/material";
 
 function LoginPage() {
   const [username, setUsername] = useState("");
@@ -11,30 +22,70 @@ function LoginPage() {
   };
 
   return (
-    <div className="login-container">
-      <h1>User Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
+    <Container component="main" maxWidth="xs">
+      <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+      >
+        <Typography component="h1" variant="h2">
+            User Login
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
             id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
             type="password"
             id="password"
+            autoComplete="current-password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+          <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+          />
+          <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+          >
+              Login
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import LoginPage from "./components/LoginPage";
 import "./styles.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const defaultTheme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <h1>User Login Page</h1>
-    <LoginPage />
+    <ThemeProvider theme={defaultTheme}>
+      <LoginPage />
+    </ThemeProvider>
   </React.StrictMode>
 );
