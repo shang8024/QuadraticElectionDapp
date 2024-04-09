789ad378be0434971f911e76da48b35883c3a5e9
Jiale Shang <shang8024@gmail.com>
2024-04-09 01:41:08 -0400

    truffle sepolia deploy config

--- truffle/migrations/1_deploy_election.js
+++ truffle/migrations/1_deploy_election.js
@@ -1,9 +1,9 @@
 const Election = artifacts.require("Election");
 const QuadraDAO = artifacts.require("QuadraDAO");
 
+const {PUBLIC_KEY} = process.env;
+
 module.exports = async function (deployer) {
-  const accounts = await web3.eth.getAccounts();
-  console.log(accounts);
-  await deployer.deploy(QuadraDAO, "0x1501C15F2349B277e6848D2dC2DB000a190608a0");
-  await deployer.deploy(Election, "0x1501C15F2349B277e6848D2dC2DB000a190608a0", QuadraDAO.address);
+  await deployer.deploy(QuadraDAO, PUBLIC_KEY);
+  await deployer.deploy(Election, PUBLIC_KEY, QuadraDAO.address);
 };
